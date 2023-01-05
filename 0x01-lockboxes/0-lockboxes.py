bcb1c0fa3e82a521ae03040ae782a62a13d4df18
mehari123 <meharithegreat@gmail.com>
2023-01-05 20:21:40 +0300

    add: g

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,18 @@
+
 #!/usr/bin/python3
+"""Lockboxes Contains method that finds the keys to
+open other lockboxes
+"""
+
+
 def canUnlockAll(boxes):
+    """
+    Function that determines if you can open all the lockboxes
+    Args:
+        boxes: list of lists of integers
+    Returns:
+        True if you can open all the lockboxes, False otherwise
+    """
     
     keys=[]
     for index,box in enumerate(boxes):
