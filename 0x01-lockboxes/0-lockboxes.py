8a65803640cdb24f9720fc2888bb88bb437c0f96
mehari123 <meharithegreat@gmail.com>
2023-01-05 20:28:49 +0300

    add:

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -14,23 +14,15 @@ def canUnlockAll(boxes):
         True if you can open all the lockboxes, False otherwise
     """
     
-   def canUnlockAll(boxes):
-    """
-    Function that determines if you can open all the lockboxes
-    Args:
-        boxes: list of lists of integers
-    Returns:
-        True if you can open all the lockboxes, False otherwise
-    """
-    unlocked = set()
-
-    for box_id, box in enumerate(boxes):
-        if len(box) == 0 or box_id == 0:
-            unlocked.add(box_id)
+    keys=[]
+    for index,box in enumerate(boxes):
         for key in box:
-            if key < len(boxes) and key != box_id:
-                unlocked.add(key)
-        if len(unlocked) == len(boxes):
-            return True
-    return False
+            if key!=index:
+                keys.append(key)
+   
+    size1=len(boxes)
+    for num in range(1,size1):
+        if num not in keys:
+            return False
+    return True
     
